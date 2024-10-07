705bc938d120934c7a79efe2ffe7b7aad5348663
FyreThread <fyrethrd@gmail.com>
2024-10-07 10:10:44 -0500

    init

---
+++ include/vex.h
@@ -0,0 +1,74 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "v5.h"
+#include "v5_vcs.h"
+using namespace vex;
+
+motor intake = motor(PORT8, ratio6_1, false);
+
+motor leftMotorA = motor(PORT1, ratio18_1, true);
+motor leftMotorB = motor(PORT2, ratio18_1, true);
+motor leftMotorC = motor(PORT3, ratio18_1, true);
+motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB, leftMotorC);
+motor rightMotorA = motor(PORT5, ratio18_1, false);
+motor rightMotorB = motor(PORT6, ratio18_1, false);
+motor rightMotorC = motor(PORT7, ratio18_1, false);
+motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB, rightMotorC);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 219, 11.5, 17.5, mm, 1.333);
+
+controller Controller1 = controller(primary);
+
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool DrivetrainNeedsToBeStopped_Controller1 = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3 + Axis4
+      // right = Axis3 - Axis4
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position() + Controller1.Axis4.position();
+      int drivetrainRightSideSpeed = Controller1.Axis3.position() - Controller1.Axis4.position();
+      
+      // check if the values are inside of the deadband range
+      if (abs(drivetrainLeftSideSpeed) < 5 && abs(drivetrainRightSideSpeed) < 5) {
+        // check if the motors have already been stopped
+        if (DrivetrainNeedsToBeStopped_Controller1) {
+          // stop the drive motors
+          LeftDriveSmart.stop();
+          RightDriveSmart.stop();
+          // tell the code that the motors have been stopped
+          DrivetrainNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the motors next time the input is in the deadband range
+        DrivetrainNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1); 
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Target7175                                                */
+/*    Created:      10/3/2024, 2:56:54 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+
+// define your global instances of motors and other devices here
+
+void intakeForward() {
+    intake.spin(forward);
+}
+void intakeReverse() {
+    intake.spin(reverse);
+}
+void intakeStop() {
+    intake.stop();
+}
+
+int main() {
+    intake.setVelocity(150, percent);
+    Controller1.ButtonR2.pressed(intakeForward);
+    Controller1.ButtonR2.released(intakeStop);
+    Controller1.ButtonL2.pressed(intakeReverse);
+    Controller1.ButtonL2.released(intakeStop);
+}
